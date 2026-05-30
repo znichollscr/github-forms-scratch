dcab8f87d5c0215c24c19c190cc7a5cc0e09817a
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2026-05-30 14:41:14 +1000

    Clean up

--- .github/GENERATE_SUMMARY/experiment.py
+++
@@ -1,9 +0,0 @@
-import cmipld
-from cmipld.utils.ldparse import name_extract
-
-me = __file__.split('/')[-1].replace('.py','')
-
-def run(io, whoami, path, name, **kwargs):
-    data = cmipld.get(f"{io}/{me}/graph.jsonld", depth=0)
-    summary = name_extract(data)
-    return f"{path}/{name}_{me}.json", me, summary
--- .github/prepublish/02-copy_context_with_extension.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-find . -type f -name "_context" -print0 | while IFS= read -r -d '' file; do
-      dir=$(dirname "$file")
-      cp "$file" "$dir/_context.json"
-done
-
-
-
